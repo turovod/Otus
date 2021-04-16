31f83e3f7b0175e4540136b4a22c9e5c0bcd16a5
Serge Loginov <s.loginov@game-insight.com>
2021-04-16 19:46:45 +0300

    add future

---
+++ main.py
@@ -0,0 +1,12 @@
+def func(a, b):
+    if a==b:
+        return a
+    elif a < b:
+        return f'{b}', func(a, b - 1)
+    elif a > b:
+        return f'{a}', func(a - 1, b)
+
+
+print(func(1, 10), end=' ')
+print()
+print(func(100, 90), end=' ')
