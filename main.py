9e47a5cd30e6bc65bd08cd8eed6bb74a533d23ee
Serge Loginov <turovodspb@gmail.com>
2021-06-12 21:45:12 +0300

    Lesson 2

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-def func(a, b):
-    if a==b:
-        return a
-    elif a < b:
-        return f'{b}', func(a, b - 1)
-    elif a > b:
-        return f'{a}', func(a - 1, b)
-
-
-print(func(1, 10), end=' ')
-print()
-print(func(100, 90), end=' ')
-
-print("aaaaaaaa")
+def test_t():
+    a = 5
+    assert a == 6, 'Hernia'
