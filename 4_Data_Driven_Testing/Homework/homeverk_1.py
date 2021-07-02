022e3b168a5aeddb7610a7136ab1ffb500d3021a
Serge Loginov <s.loginov@game-insight.com>
2021-07-02 19:31:14 +0300

    Added lesson 4

---
+++ 4_Data_Driven_Testing/Homework/homeverk_1.py
@@ -0,0 +1,48 @@
+with open('data.txt', 'rb', ) as f:
+    data = f.read()
+
+    data = data.decode('utf-8').replace('\r', '').replace('\n', '').replace('\xa0', '') \
+        .replace('1', '').replace('0', '')
+
+    ls = data.split(',')
+
+    for item in ls:
+        if item == '':
+            ls.remove(item)
+
+        if item == '':
+            ls.remove(item)
+
+        if item == '':
+            ls.remove(item)
+
+    ls = ls[5:]
+
+    ls_pipol_citis = []
+    sw: str
+
+for i in range(len(ls) - 1):
+    if i % 2 == 0:
+        sw = ls[i + 1], ls[i]
+        ls_pipol_citis.append(','.join(sw))
+
+for i in range(len(ls_pipol_citis)):
+    ls_pipol_citis[i] = ls_pipol_citis[i].replace(',', ' ')
+print(ls_pipol_citis)
+s = '-+'
+
+gen = (i + ' ' + j + ' ' + k + ' ' + f for i in ls_pipol_citis for j in s for k in s for f in s)
+
+with open('result.txt', 'w', encoding='utf-8') as f:
+    for i in range(100):
+        f.write(next(gen) + '\n')
+
+#
+# for i in range(100):
+#     print(next(gen))
+
+
+
+#
+# for line in data:
+#     print(line)
--- 4_Data_Driven_Testing/Loops/example1-loops.py
+++ 4_Data_Driven_Testing/Loops/example1-loops.py
@@ -1,11 +1,11 @@
 # Prints out 0,1,2,3,4 and then it prints "count value reached 5"
 
-count = 0
-while count < 5:
-    print(count)
-    count += 1
-else:
-    print("count value reached %d" % count)
+# count = 0
+# while count < 5:
+#     print(count)
+#     count += 1
+# else:
+#     print("count value reached %d" % count)
 
 # Prints out 1,2,3,4
 for i in range(1, 10):
