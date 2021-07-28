497a8079d185cd63adae01be9c35d457792f3e9c
Serge Loginov <s.loginov@game-insight.com>
2021-07-28 19:27:42 +0300

    Added lesson 7

--- 6_find_element/Test_Opencart/conftest.py
+++ 6_find_element/Test_Opencart/conftest.py
@@ -5,12 +5,12 @@ from selenium import webdriver
 @pytest.fixture
 def initial_test(request):
     if request.config.getoption('--brouser') == 'C':
-        driver = webdriver.Chrome(executable_path='/Users/s.loginov/Desktop/Otus/5_Test_Opencart/drivers/chromedriver')
+        driver = webdriver.Chrome(executable_path='/5_Test_Opencart/drivers/chromedriver')
         driver.get('http://localhost:8888/opencart3/upload/')
         driver.fullscreen_window()
 
     if request.config.getoption('--brouser') == 'F':
-        driver = webdriver.Firefox(executable_path='/Users/s.loginov/Desktop/Otus/5_Test_Opencart/drivers/geckodriver')
+        driver = webdriver.Firefox(executable_path='/5_Test_Opencart/drivers/geckodriver')
         driver.get('http://localhost:8888/opencart3/upload/')
 
     yield driver
