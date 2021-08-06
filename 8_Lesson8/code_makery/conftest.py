26d106adaeb521c4b65ed48e5a108e8d124deda8
Serge Loginov <s.loginov@game-insight.com>
2021-08-06 19:59:10 +0300

    Added lesson 9

--- 8_Lesson8/code_makery/conftest.py
+++ 8_Lesson8/code_makery/conftest.py
@@ -8,7 +8,7 @@ from selenium import webdriver
 def set_up(request):
     webdriver_path = __file__.replace('conftest.py', '') + 'drivers/chromedriver'
     wd = webdriver.Chrome(executable_path=webdriver_path)
-    wd.get('https://www.html5rocks.com/ru/tutorials/dnd/basics//')
+    wd.get('https://html5css.ru/html/html5_draganddrop.php')
 
     wd.fullscreen_window()
     request.addfinalizer(wd.close)
--- 8_Lesson8/code_makery/test_action_chains.py
+++ 8_Lesson8/code_makery/test_action_chains.py
@@ -5,9 +5,11 @@ from selenium.webdriver.common.action_chains import ActionChains
 def test_action_chains(set_up):
     wd = set_up
 
-    wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    anotation_body = wd.find_element_by_id('drag1')
+    alink_from = wd.find_element_by_id("div2")
 
-    ActionChains(driver).click(anotation_body).drag_and_drop(alink_from, anotation_body).perform()
-    wd.find_element_by_css_selector('#columns-full > div:nth-child(1) > div').click()
+    time.sleep(3)
+
+    ActionChains(wd).click(alink_from).drag_and_drop(source=anotation_body, target=alink_from).perform()
 
     time.sleep(5)
